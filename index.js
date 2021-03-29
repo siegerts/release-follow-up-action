31120860970a68f71d314d3d46b2004ae7de8186
siegerts <stephen.siegert@gmail.com>
2021-03-29 14:14:23 -0400

    update template and port over script logic

--- index.js
+++ index.js
@@ -1,20 +1,123 @@
-const core = require('@actions/core');
-const wait = require('./wait');
+const github = require("@actions/github");
+const core = require("@actions/core");
 
-
-// most @actions toolkit packages have async methods
+/**
+ * Follow up on issues and PRs, open or closed/merged,
+ * after a release is published. The `pending-release`
+ * label will also be removed if it's present.
+ *
+ * The relevant issues are parsed from the release
+ * changelog and only recognized if present in the
+ * current repo (i.e. where the release is published).
+ *
+ * @param {string} `github-token`
+ * @param {boolean} `dry-run`
+ *
+ */
 async function run() {
-  try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
+  const token = core.getInput("github-token", { required: true });
+  // const dryRun = core.getInput("dry-run");
 
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
+  const octo = github.getOctokit(token);
 
-    core.setOutput('time', new Date().toTimeString());
-  } catch (error) {
+  // context
+  const context = octo.context;
+  const repo = context.repo;
+  const release = context.payload.release;
+
+  if (!repo || !release) {
     core.setFailed(error.message);
+  }
+
+  // release
+  const releaseName = release.tag_name;
+  const releaseBody = release.body;
+
+  // parse release notes for issue (includes PR)
+  // numbers in the current repo
+  const issuePattern = `\/${repo.repo}\/issues\/([0-9]+)+?`;
+  const issueRE = new RegExp(issuePattern, "gim");
+
+  // match and dedupe
+  const matches = new Set(
+    [...releaseBody.matchAll(issueRE)].map((issue) => issue[1])
+  );
+  const issues = [...matches];
+
+  if (Array.isArray(issues) && issues.length) {
+    core.info(
+      `Issues referenced in the ${releaseName} release: ${issues.join(", ")}`
+    );
+
+    for (const issueNumber of issues) {
+      core.info(`Updating issue #${issueNumber}...`);
+
+      let issue;
+      try {
+        issue = await octo.issues.get({
+          owner: repo.owner,
+          repo: repo.repo,
+          issue_number: issueNumber,
+        });
+      } catch (err) {
+        core.info(
+          `Referenced issue (#${issueNumber}) cannot be found. Skipping...`
+        );
+        continue;
+      }
+
+      // update open and *closed* issues/PRs
+      if (issue && issue.status === 200) {
+        const issueData = issue.data;
+
+        const labels = issueData.labels || [];
+
+        // https://docs.github.com/en/rest/reference/issues#get-an-issue
+        // GitHub's REST API v3 considers every pull request an
+        // issue, but not every issue is a pull request.
+        const issueTypeName = Object.keys(issueData).includes("pull_request")
+          ? "pull request"
+          : "issue";
+
+        const isPendingRelease = labels.some(
+          ({ name }) => name === "pending-release"
+        );
+
+        if (isPendingRelease) {
+          await octo.issues.removeLabel({
+            owner: repo.owner,
+            repo: repo.repo,
+            issue_number: issueNumber,
+            name: "pending-release",
+          });
+        }
+
+        await octo.issues.addLabels({
+          owner: repo.owner,
+          repo: repo.repo,
+          issue_number: issueNumber,
+          labels: ["referenced-in-release"],
+        });
+
+        const commentBody = `
+   👋 Hi, this ${issueTypeName} was referenced in the **${releaseName}** release!\n\n \
+
+   Check out the release notes here ${release.html_url}.
+         `;
+        await octo.issues.createComment({
+          issue_number: issueNumber,
+          owner: repo.owner,
+          repo: repo.repo,
+          body: commentBody,
+        });
+      } else {
+        core.info(
+          `Referenced issue (#${issueNumber}) does not exist. Skipping...`
+        );
+      }
+    }
+  } else {
+    core.info(`No issues were referenced in this release. Skipping...`);
   }
 }
 
